dde13cc4cc18c24819f7d22f5b7343fc7819cd37
AdrianoMaia <drikomaia89@gmail.com>
2022-05-25 09:16:29 -0300

    Adicionando interface

--- script.ts
+++ script.ts
@@ -1,14 +1,48 @@
-(function(){
-    const $ = (query:string): HTMLInputElement | null => document.querySelector(query);
+interface Veiculo {
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
 
-    $("#cadastrar")?.addEventListener("click",() => {
+(function () {
+    const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function patio() {
+
+        function ler() { }
+
+        function adicionar(veiculo: Veiculo) {
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+                <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+        }
+
+        function remover() { }
+
+        function salvar() { }
+
+        function render() { }
+
+        return { ler, adicionar, remover, salvar, render }
+    }
+
+    $("#cadastrar")?.addEventListener("click", () => {
         const nome = $("#nome")?.value;
         const placa = $("#placa")?.value;
 
-        if(!nome || !placa){
+        if (!nome || !placa) {
             alert("Os campos acima são obrigatórios");
             return;
         }
+
+        patio().adicionar({nome, placa, entrada: new Date()})
     })
 
 })();
